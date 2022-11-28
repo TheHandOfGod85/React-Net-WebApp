7aba3ad71005ce6a869aca1c486a0ca83ac5af74
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-11-28 13:57:55 +0000

    implemented formik

---
+++ client-app/src/app/common/form/options/categoryOptions.ts
@@ -0,0 +1,26 @@
+export const categoryOptions = [
+  {
+    text: "Drinks",
+    value: "drink",
+  },
+  {
+    text: "Culture",
+    value: "culture",
+  },
+  {
+    text: "Film",
+    value: "film",
+  },
+  {
+    text: "Food",
+    value: "food",
+  },
+  {
+    text: "Music",
+    value: "music",
+  },
+  {
+    text: "Travel",
+    value: "travel",
+  },
+];
